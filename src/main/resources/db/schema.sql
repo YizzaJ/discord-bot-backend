e25c53cf4a296e16b8ac5af856d5b1008888631e
Jesus Hernandez <jesusdavidhernandez.perez@gmail.com>
2023-03-06 10:02:22 +0100

    Changes to schema

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -3,46 +3,39 @@ DROP DATABASE IF EXISTS NEWSBOTDB;
 CREATE DATABASE NEWSBOTDB;
 USE NEWSBOTDB;
 
+CREATE TABLE Provider (
+    nombre varchar(255) NOT NULL,
+    link varchar(255) NOT NULL,
+
+    PRIMARY KEY (link)
+);
+CREATE TABLE User(
+	ID int NOT NULL auto_increment,
+    username varchar(255) NOT NULL,
+    provider varchar(255) NOT NULL,
+    
+    PRIMARY KEY (ID, username),
+    FOREIGN KEY (provider) REFERENCES Provider(link)
+);
+
 CREATE TABLE Property (
-    ID int NOT NULL,
+	provider varchar(255) NOT NULL,
 	uso varchar(255) NOT NULL,
     tipo varchar(255) NOT NULL,
     attributeName varchar(255),
     valor varchar(255) NOT NULL,
 
-    PRIMARY KEY (ID)
+    PRIMARY KEY (provider, uso),
+    FOREIGN KEY (provider) REFERENCES Provider(link)
 );
 
-CREATE TABLE ScrapingProperties (
-    ID int NOT NULL,
-	article int NOT NULL,
-    firstParagraph int NOT NULL,
-    topic int NOT NULL,
-    
-    PRIMARY KEY (ID),
-    FOREIGN KEY (article) REFERENCES Property(ID),
-    FOREIGN KEY (firstParagraph) REFERENCES Property(ID),
-    FOREIGN KEY (topic) REFERENCES Property(ID)
-);
 
-CREATE TABLE Provider (
-    ID int NOT NULL,
-    nombre varchar(255) NOT NULL,
-    link varchar(80),
-    properties int NOT NULL,
-    
-    PRIMARY KEY (ID),
-    FOREIGN KEY (properties) REFERENCES ScrapingProperties(ID)
-);
 
-CREATE TABLE User(
-    ID int NOT NULL,
-    username varchar(255) NOT NULL,
-    provider int NOT NULL,
-    
-    PRIMARY KEY (ID),
-    FOREIGN KEY (provider) REFERENCES Provider(ID)
-);
+
+
+
+
+
 
 
 
--- src/main/resources/db/schemaActual.sql
+++ src/main/resources/db/schemaActual.sql
@@ -6,28 +6,39 @@ USE NEWSBOTDB;
 CREATE TABLE Provider (
     nombre varchar(255) NOT NULL,
     link varchar(255) NOT NULL,
+	uso_articulo varchar(255) NOT NULL,
+    tipo_articulo varchar(255) NOT NULL,
+    attributeName_articulo varchar(255),
+    valor_articulo varchar(255) NOT NULL,
+	uso_parrafo varchar(255) NOT NULL,
+    tipo_parrafo varchar(255) NOT NULL,
+    attributeName_parrafo varchar(255),
+    valor_parrafo varchar(255) NOT NULL,
+	uso_topic varchar(255) NOT NULL,
+    tipo_topic varchar(255) NOT NULL,
+    attributeName_topic varchar(255),
+    valor_topic varchar(255) NOT NULL,
 
     PRIMARY KEY (link)
 );
 CREATE TABLE User(
 	ID int NOT NULL auto_increment,
     username varchar(255) NOT NULL,
+    servername varchar(255) NOT NULL,
     provider varchar(255) NOT NULL,
-    
+
     PRIMARY KEY (ID, username),
-    FOREIGN KEY (provider) REFERENCES Provider(link)
+    FOREIGN KEY (provider) REFERENCES Provider(link),
+    FOREIGN KEY (servername) REFERENCES Server(servername)
 );
 
-CREATE TABLE Property (
-	provider varchar(255) NOT NULL,
-	uso varchar(255) NOT NULL,
-    tipo varchar(255) NOT NULL,
-    attributeName varchar(255),
-    valor varchar(255) NOT NULL,
+CREATE TABLE Server(
+	ID int NOT NULL,
+    servername varchar(255) NOT NULL,
 
-    PRIMARY KEY (provider, uso),
-    FOREIGN KEY (provider) REFERENCES Provider(link)
+    PRIMARY KEY (ID)
 );
+
 
 
 
